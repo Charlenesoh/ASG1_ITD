12c7fa0c274b82b2735eb51045454ea5361de1af
Charlenesoh <74208691+Charlenesoh@users.noreply.github.com>
2021-11-23 20:02:08 +0800

    quiz
    quiz creation

--- Assets/Scripts/QuizManager.cs
+++ Assets/Scripts/QuizManager.cs
@@ -13,7 +13,28 @@ public class QuizManager : MonoBehaviour
 
     private void Start()
     {
-        
+        generateQuestions();
+    }
+
+
+    public void Correct()
+    {
+        QnA.RemoveAt(currentQuestions);
+        generateQuestions();
+    }
+    void SetAnswers()
+    {
+        for ( int i = 0; i < options.Length; i++)
+        {
+            options[i].GetComponent<AnswerScript>().isCorrect = false;
+            options[i].transform.GetChild(0).GetComponent<Text>().text = QnA[currentQuestions].Answers[i];
+
+            if (QnA[currentQuestions].CorrectAnswer == i + 1)
+            {
+                options[i].GetComponent<AnswerScript>().isCorrect = true;
+            }
+
+        }
     }
 
     void generateQuestions()
@@ -21,5 +42,9 @@ public class QuizManager : MonoBehaviour
         currentQuestions = Random.Range(0, QnA.Count);
 
         QuestionTxt.text = QnA[currentQuestions].Questions;
+
+        SetAnswers();
+
+        
     }
 }
