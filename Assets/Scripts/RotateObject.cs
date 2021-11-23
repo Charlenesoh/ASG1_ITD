89d65b8a352e84bf41ac782f820fb1037eda22c2
Charlenesoh <74208691+Charlenesoh@users.noreply.github.com>
2021-11-23 12:38:04 +0800

    rotation script
    For objects to rotate

---
+++ Assets/Scripts/RotateObject.cs
@@ -0,0 +1,36 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RotateObject : MonoBehaviour
+{
+    private Vector3 rotation;
+    [SerializeField] private float speed;
+
+    private void Update()
+    {
+        if (Input.GetKey(KeyCode.W))
+        {
+            rotation = Vector3.up;
+        }
+
+        else if (Input.GetKey(KeyCode.S))
+        {
+            rotation = Vector3.down;
+        }
+
+        else if (Input.GetKey(KeyCode.A))
+        {
+            rotation = Vector3.left;
+        }
+
+        else if (Input.GetKey(KeyCode.D))
+        {
+            rotation = Vector3.right;
+        }
+
+        else rotation = Vector3.zero;
+
+        transform.Rotate(rotation * speed * Time.deltaTime);
+    }
+}
