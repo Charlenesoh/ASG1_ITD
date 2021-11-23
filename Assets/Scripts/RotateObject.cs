1742385ef4f988b502657e8356fa2891222441f8
Charlenesoh <74208691+Charlenesoh@users.noreply.github.com>
2021-11-23 16:17:46 +0800

    Rotation Completed
    Rotation script Final

--- Assets/Scripts/RotateObject.cs
+++ Assets/Scripts/RotateObject.cs
@@ -3,11 +3,12 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class RotateObject : MonoBehaviour
-{
+{ 
     private Vector3 rotation;
     [SerializeField] private float speed;
+    public GameObject Object;
 
-    private void Update()
+     /*void Update()
     {
         if (Input.GetKey(KeyCode.W))
         {
@@ -32,5 +33,17 @@ public class RotateObject : MonoBehaviour
         else rotation = Vector3.zero;
 
         transform.Rotate(rotation * speed * Time.deltaTime);
+    }*/
+
+    public void Vertical()
+    {
+        rotation = Vector3.up;
+        Object.transform.Rotate(rotation * speed * Time.deltaTime);
     }
+    public void Horizontal()
+    {
+        rotation = Vector3.left;
+        Object.transform.Rotate(rotation * speed * Time.deltaTime);
+    }
+
 }
